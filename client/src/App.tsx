924e5365694519c902e63d29f280e51c09351a91
Ashish Malla <ashishmalla45@gmail.com>
2022-12-31 12:43:31 -0700

    initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+	return (
+		<div>
+			<h1 className='text-center text-8xl font-extrabold text-[#05B0A1] drop-shadow-2xl'>
+				GetDone
+			</h1>
+		</div>
+	)
+}
+
+export default App
