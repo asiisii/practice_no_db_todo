2c71632d479ce8b8cf447f9f5ded0e0fd8e3d4a2
Ashish Malla <ashishmalla45@gmail.com>
2023-01-02 13:43:23 -0700

    remove unused files

--- client/src/redux/api/baseApiSlice.ts
+++
@@ -1,9 +0,0 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-export const baseApiSlice = createApi({
-	reducerPath: 'todos',
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:3000/',
-	}),
-	endpoints: () => ({}),
-})
