caf3c95858b7e550d2964526f925b903cb9f071c
Ashish Malla <ashishmalla45@gmail.com>
2023-01-02 14:53:52 -0700

    setup redux store to see data in the DevTools extension

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ApiProvider } from '@reduxjs/toolkit/query/react'
+import { store } from './app/store'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { todoApiSlice } from './features/api/todosApiSlice'
 import App from './App'
 import './main.css'
+
+store.dispatch(todoApiSlice.endpoints.getTodo.initiate())
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
