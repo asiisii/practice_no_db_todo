a998d503a0fdebf32f1842603a2bc3860a3ccaa6
Ashish Malla <ashishmalla45@gmail.com>
2023-01-02 14:55:36 -0700

    setup redux store to use the devtools extension

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ApiProvider } from '@reduxjs/toolkit/query/react'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 import { todoApiSlice } from './features/api/todosApiSlice'
