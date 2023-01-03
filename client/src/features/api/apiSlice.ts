9c0a7dca8511d8d0ee8ede32a507de674a513954
Ashish Malla <ashishmalla45@gmail.com>
2023-01-03 00:02:51 -0700

    debugging

--- client/src/features/api/apiSlice.ts
+++ client/src/features/api/apiSlice.ts
@@ -1,11 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const BASE_URL =
-	'https://musical-snickerdoodle-afc0db.netlify.app/' || 'http://localhost:3000'
+const BaseURL = process.env.BASE_URL || 'http://localhost:3000'
 
 export const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+	baseQuery: fetchBaseQuery({ baseUrl: BaseURL }),
 	tagTypes: ['Todos'],
 	endpoints: () => ({}),
 })
