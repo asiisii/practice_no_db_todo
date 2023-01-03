ef09b91b70d168a16f312c0d570b7938c48241a4
Ashish Malla <ashishmalla45@gmail.com>
2023-01-03 00:07:38 -0700

    debugging

--- client/src/features/api/apiSlice.ts
+++ client/src/features/api/apiSlice.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const BaseURL = process.env.BASE_URL || 'http://localhost:3000'
+console.log({baseurl: BaseURL, en: process.env.BASE_URL});
 
 export const apiSlice = createApi({
 	reducerPath: 'api',
